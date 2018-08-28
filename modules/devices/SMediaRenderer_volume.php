1bb67358297041638a5c1f4fb80e8aa163ccd9ba
tarasfrompir <tarasfrompir@inbox.ru>
2018-08-28 14:45:35 +0300

    Update SMediaRenderer_volume.php

--- modules/devices/SMediaRenderer_volume.php
+++ modules/devices/SMediaRenderer_volume.php
@@ -5,6 +5,6 @@ require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/autoload.php');
 use jalder\Upnp\MediaRenderer;
 
 $adress = $this->getProperty("CONTROLADDRESS");
-$remote = new MediaRenderer\Remote($adress);
+$remote = new MediaRenderer\Remote1($adress);
 $volume = $this->getProperty("volume");
 $result = $remote->SetVolume($volume);
