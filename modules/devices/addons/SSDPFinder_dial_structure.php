8c89ea8d51bfbc239b92af5349193de48aebbe5d
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-11 18:51:15 +0300

    Update SSDPFinder_dial_structure.php

--- modules/devices/addons/SSDPFinder_dial_structure.php
+++ modules/devices/addons/SSDPFinder_dial_structure.php
@@ -5,7 +5,22 @@ $this->device_types['dial'] = array(
         'PARENT_CLASS'=>'UPNPdevices',
         'CLASS'=>'Sdial',
         'PROPERTIES'=>array(
-            'PlayURL'=>array('DESCRIPTION'=>' Play Url','ONCHANGE'=>'updatePreview','_CONFIG_TYPE'=>'text'),
             'Applications'=>array('DESCRIPTION'=>' Приложения','_CONFIG_TYPE'=>'text'),
+            'seeknext'=>array('DESCRIPTION'=>'Перемотка вперед на 30сек', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'seeknext', 'DATA_KEY'=>1),
+            'seekprevious'=>array('DESCRIPTION'=>'Перемотка назад на 30сек', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'seekprevious', 'DATA_KEY'=>1),
+            'stop'=>array('DESCRIPTION'=>'Стоп', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'stop', 'DATA_KEY'=>1),
+            'playUrl'=>array('DESCRIPTION'=>'Воспроизвести ссылку', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'playUrl', 'DATA_KEY'=>1),
+            'mute_unmute'=>array('DESCRIPTION'=>'Отключение/включение звука', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'mute-unmute', 'DATA_KEY'=>1),
+            'volume'=>array('DESCRIPTION'=>'Уровень звука', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'volume', 'DATA_KEY'=>1),
+            'pause_unpause'=>array('DESCRIPTION'=>'Отключение/включение паузы', 'KEEP_HISTORY'=>1, 'ONCHANGE'=>'pause-unpause', 'DATA_KEY'=>1),
         ),
+        'METHODS'=>array(
+            'mute-unmute'=>array('DESCRIPTION'=>'Отключение/включение звука'),
+            'volume'=>array('DESCRIPTION'=>'Уровень звука'),
+            'pause-unpause'=>array('DESCRIPTION'=>'Отключение/включение паузы'),
+            'seeknext'=>array('DESCRIPTION'=>'Перемотка вперед на 30сек'),
+            'seekprevious'=>array('DESCRIPTION'=>'Перемотка назад на 30сек'),
+            'stop'=>array('DESCRIPTION'=>'Стоп'),
+            'playUrl'=>array('DESCRIPTION'=>'Воспроизвести ссылку'),
+        )
 );
