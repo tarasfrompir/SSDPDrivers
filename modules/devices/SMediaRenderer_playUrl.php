34279ef96cb391ed27307246fcbb37bfed0b7d56
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-14 21:56:52 +0300

    Update SMediaRenderer_playUrl.php

--- modules/devices/SMediaRenderer_playUrl.php
+++ modules/devices/SMediaRenderer_playUrl.php
@@ -2,9 +2,12 @@
 require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/autoload.php');
 use jalder\Upnp\MediaRenderer;
 $renderer = new MediaRenderer();
+
 $adress = $this->getProperty("CONTROLADDRESS");
 $remote = new MediaRenderer\Remote($adress);
 $playUrl = $this->getProperty("playUrl");
+$services = $this->getProperty("Services");
+
 $info = $remote->getPosition();
 $doc = new \DOMDocument();
 $doc->loadXML($info);
