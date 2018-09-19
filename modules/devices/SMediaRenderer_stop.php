9f6cb371185b52b6e838883be67890ea6d55fcab
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-19 09:59:21 +0300

    Update SMediaRenderer_stop.php

--- modules/devices/SMediaRenderer_stop.php
+++ modules/devices/SMediaRenderer_stop.php
@@ -2,8 +2,7 @@
 
 require(dirname(__FILE__).'/../ssdp_finder/upnp/vendor/autoload.php');
 
-use jalder\Upnp\Renderer;
-
+use jalder\Upnp\MediaRenderer;
 $renderer = new MediaRenderer();
 
 $adress = $this->getProperty("CONTROLADDRESS");
