03c3f4f2228da581ce5cc2c9c2fb429b6167bad6
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-29 08:58:29 +0300

    Add files via upload

---
+++ modules/devices/SMagXXXdevice_tv.php
@@ -0,0 +1,8 @@
+<?
+require_once(dirname(__FILE__)."/../ssdp_finder/upnp/vendor/jalder/upnp/src/MagXXXdevice/Remote.php");
+$adress = $this->getProperty("CONTROLADDRESS");
+$password = $this->getProperty("PASSWORD");
+
+// Create Chromecast object and give IP and Port
+$mag = new MAG250();
+$mag->key_tv($adress, $password);
