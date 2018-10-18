55f8400174d9f7f07dc5a02a3d991468b4ba8229
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-18 14:59:13 +0300

    Update SMediaRenderer_playUrl.php

--- modules/devices/SMediaRenderer_playUrl.php
+++ modules/devices/SMediaRenderer_playUrl.php
@@ -7,19 +7,6 @@ $adress = $this->getProperty("CONTROLADDRESS");
 $remote = new MediaRenderer\Remote($adress);
 
 $playUrl = $this->getProperty("playUrl");
-$services = $this->getProperty("Services");
-
-$info = $remote->getPosition();
-$doc = new \DOMDocument();
-$doc->loadXML($info);
-$result = $doc->getElementsByTagName('Track');
-$trackn = $result->item(0)->nodeValue;
-DebMes ($trackn);       
-$result = $doc->getElementsByTagName('TrackURI');
-$trackurl = $result->item(0)->nodeValue;
-DebMes($trackurl); 
-$result = $doc->getElementsByTagName('RelTime');
-$tracktime = $result->item(0)->nodeValue;
 DebMes($tracktime); 
 DebMes($playUrl); 
 DebMes($adress); 
