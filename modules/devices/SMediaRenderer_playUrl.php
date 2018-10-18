4579bf4bb088743c648b478aae879658f5f867f1
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-18 14:28:28 +0300

    Update SMediaRenderer_playUrl.php

--- modules/devices/SMediaRenderer_playUrl.php
+++ modules/devices/SMediaRenderer_playUrl.php
@@ -5,6 +5,7 @@ $renderer = new MediaRenderer();
 
 $adress = $this->getProperty("CONTROLADDRESS");
 $remote = new MediaRenderer\Remote($adress);
+echo ($remote);
 $playUrl = $this->getProperty("playUrl");
 $services = $this->getProperty("Services");
 
@@ -13,21 +14,15 @@ $doc = new \DOMDocument();
 $doc->loadXML($info);
 $result = $doc->getElementsByTagName('Track');
 $trackn = $result->item(0)->nodeValue;
-print ($trackn);       
+echo ($trackn);       
 $result = $doc->getElementsByTagName('TrackURI');
 $trackurl = $result->item(0)->nodeValue;
-print ($trackurl); 
+echo($trackurl); 
 $result = $doc->getElementsByTagName('RelTime');
 $tracktime = $result->item(0)->nodeValue;
-print ($tracktime); 
-print ($playUrl); 
-print ($adress); 
-if (strpos($playUrl,'youtube')>1 AND strpos($playUrl,'youtube')<20) {
-    $res1=parse_url($playUrl, PHP_URL_QUERY);
-    $res2=parse_str($res1,$res); 
-    $res=$res['v'];
-    $newurl='https://hms.lostcut.net/youtube/g.php?v='.$res.'&link_only=1&max_height=720'; 
-    $playUrl=file_get_contents($newurl);
-};
+echo ($tracktime); 
+echo ($playUrl); 
+echo ($adress); 
 $result = $remote->stop();
 $result = $remote->play($playUrl);
+echo ($result);
