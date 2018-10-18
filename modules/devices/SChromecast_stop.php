b42cb3b7ee827f869cc23d6aacdeb2be0bbbe51b
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-18 08:43:15 +0300

    Update SChromecast_stop.php

--- modules/devices/SChromecast_stop.php
+++ modules/devices/SChromecast_stop.php
@@ -2,21 +2,39 @@
 require_once(dirname(__FILE__)."/../ssdp_finder/upnp/vendor/jalder/upnp/src/Chromecast/Remote.php");
 
 $adress = $this->getProperty("CONTROLADDRESS");
-$ip = getIp($adress,false);
-
+$port = getport($adress);
+$ip = getIp($adress, false);
 // Create Chromecast object and give IP and Port
-$cc = new Chromecast($ip,"8009");
+$cc = new Chromecast($ip, $port);
 
 $cc->DMP->Stop();
 
-function getIp($baseUrl,$withPort) {
-	if( !empty($baseUrl) ){
+function getIp($baseUrl, $withPort)
+    {
+    if (!empty($baseUrl))
+        {
         $parsed_url = parse_url($baseUrl);
-        if($withPort ==true){
-            $baseUrl = $parsed_url['scheme'].'://'.$parsed_url['host'].':'.$parsed_url['port']; 
-           }else{
+        if ($withPort == true)
+            {
+            $baseUrl = $parsed_url['scheme'] . '://' . $parsed_url['host'] . ':' . $parsed_url['port'];
+            }
+          else
+            {
             $baseUrl = $parsed_url['host'];
+            }
         }
+    return $baseUrl;
     }
+/**
+* get port from url
+*
+* @access public
+*/
+ function getPort($address){
+  $baseUrl="";
+	if( !empty($address) ){
+        $parsed_url = parse_url($address);
+        $baseUrl = $parsed_url['port'];
+        }
     return  $baseUrl;
 }
