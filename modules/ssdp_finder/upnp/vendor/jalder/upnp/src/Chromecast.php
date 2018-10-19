836621af4e612e0ad52fdac2d0a7d87d7bc8be73
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-19 11:07:02 +0300

    Update Chromecast.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Chromecast.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/Chromecast.php
@@ -30,12 +30,7 @@ class Chromecast
 	public $lastactivetime;
 	// store the time we last did something
 	
-	function cl_print_r ($var, $label = '')
-{
-	$str = json_encode(print_r ($var, true));
-	echo "<script>console.group('".$label."');console.log('".$str."');console.groupEnd();</script>";
-}
-	
+
 	public function __construct($ip, $port)
 	{
 		// Establish Chromecast connection
