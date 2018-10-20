1ea077e9bfea1c5abb66be7b870eb65147478ae0
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-20 21:47:12 +0300

    Update Remote.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
@@ -56,44 +56,29 @@ class Remote
 	//this should be moved to the upnp and renderer model
 	public function getControlURL($description_url, $service = 'AVTransport') {
 		$description = $this->getDescription($description_url);
-
-		switch($service)
-		{
-			case 'AVTransport':
-				$serviceType = $this->service_type;
-				break;
-			default:
-				$serviceType = $this->service_type;
-				break;
-		}
-
-		foreach($description['device']['serviceList']['service'] as $service) {
-			if($service['serviceType'] == $serviceType) {
+                foreach($description['device']['serviceList']['service'] as $service) {
+  			if($service['serviceType'] == $this->service_type) {
 				$url = parse_url($description_url);
 				return $url['scheme'].'://'.$url['host'].':'.$url['port'].$service['controlURL'];
 			}
 		}
 	}
 
-	public function getState()
-	{
+	public function getState() {
 		return $this->instanceOnly('GetTransportInfo');
 	}
 
-	public function getPosition()
-	{
+	public function getPosition() {
 		return $this->instanceOnly('getPositionInfo');
 	}
 
-	private function instanceOnly($command,$type = 'AVTransport', $id = 0)
-	{
+	private function instanceOnly($command,$type = 'AVTransport', $id = 0) {
 		$args = array('InstanceID'=>$id);
 		$response = $this->sendRequestToDevice($command,$args,$this->ctrlurl,$this->service_type);
         return $response;
 	}
 
-	public function getMedia()
-	{
+	public function getMedia() {
 		$response = $this->instanceOnly('GetMediaInfo');
 		// сохраняет данные в файл
 		//$file = 'people.txt';
@@ -109,44 +94,36 @@ class Remote
 			}
 		return $track;
 	}
-	public function stop()
-	{
+	public function stop() {
 		return $this->instanceOnly('Stop');
 	}
 	
-	public function unpause()
-	{
+	public function unpause() {
 		$args = array('InstanceID'=>0,'Speed'=>1);
 		return $this->sendRequestToDevice('Play',$args,$this->ctrlurl,$this->service_type);     
 	}
 
-	public function pause()
-	{
+	public function pause() {
 		return $this->instanceOnly('Pause');
 	}
 
-	public function next()
-	{
+	public function next() {
 		return $this->instanceOnly('Next');
 	}
 
-	public function previous()
-	{
+	public function previous() {
 		return $this->instanceOnly('Previous');
 	}
 
-        public function fforward()
-        {
+        public function fforward() {
                return $this->next();
         }
 
-        public function rewind()
-        {
+        public function rewind() {
                return $this->previous();
         }
 
-	public function seek($unit = 'TRACK_NR', $target=0)
-	{
+	public function seek($unit = 'TRACK_NR', $target=0) {
 		$response = $this->sendRequestToDevice('Seek',$args,$this->ctrlurl.'serviceControl/AVTransport','AVTransport');
 		return $response['s:Body']['u:SeekResponse'];
 	}
@@ -163,8 +140,7 @@ class Remote
 	    return $response;
 	}
 	
-        private function sendRequestToDevice($method, $arguments, $url, $type)
-        {
+        private function sendRequestToDevice($method, $arguments, $url, $type) {
         $body  ='<?xml version="1.0" encoding="utf-8"?>' . "\r\n";
         $body .='<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">';
         $body .='<s:Body>';
@@ -175,7 +151,7 @@ class Remote
         $body .='</u:'.$method.'>';
         $body .='</s:Body>';
         $body .='</s:Envelope>';
-        var_dump ($body);
+        echo ($body);
         var_dump ('ST '.$this->service_type);
 	$header = array(
 	    'Host: '.$this->ip.':'.$this->port,
