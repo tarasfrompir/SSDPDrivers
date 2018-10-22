87a866d4f75b05a7c03f7c1cc53d65d4ed8417fe
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-22 20:06:10 +0300

    Update Remote.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
@@ -93,14 +93,15 @@ class Remote {
         if ($url === "") {
             return self::unpause();
         }
-        $meta = '<?xml version="1.0"?> <DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sec="http://www.sec.co.kr/">
-                  <item id="f-0" parentID="0" restricted="0">
-    <dc:title>Video</dc:title>
-    <dc:creator>Anonymous</dc:creator>
-    <upnp:class>object.item.videoItem</upnp:class>
-    <res protocolInfo="http-get:*:audio/mpeg:DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01700000000000000000000000000000">{0}</res>
-</item>
-</DIDL-Lite>';
+        $meta = '<?xml version="1.0"?>
+	<DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sec="http://www.sec.co.kr/">
+         <item id="f-0" parentID="0" restricted="0">
+         <dc:title>Video</dc:title>
+         <dc:creator>Anonymous</dc:creator>
+         <upnp:class>object.item.audioItem</upnp:class>
+         <res protocolInfo="http-get:*:audio/mpeg:DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01700000000000000000000000000000">{0}</res>
+         </item>
+       </DIDL-Lite>';
         $args = array('InstanceID' => '0', 'CurrentURI' => '<![CDATA[' . $url . ']]>', 'CurrentURIMetaData' => $meta);
         $response = $this->sendRequestToDevice('SetAVTransportURI', $args);
         //var_dump($response);
