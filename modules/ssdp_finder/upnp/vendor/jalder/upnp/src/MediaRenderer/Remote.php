7dd53beb2a1e88a6bfdb73a3fadc5df96378514e
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-22 13:26:32 +0300

    Update Remote.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
@@ -113,14 +113,7 @@ public function setNext($url) {
                 return self::unpause();
                 }
         $rand = mt_rand(10000000, 99999999);
-        $meta = '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" 
-                    xmlns:r="urn:schemas-rinconnetworks-com:metadata-1-0/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/">
-                 <item id="'.$rand.'" restricted="true">
-                    <dc:title>Majordomo play url command</dc:title>
-                    <upnp:class>object.item.audioItem</upnp:class>
-                    <desc id="cdudn" nameSpace="urn:schemas-rinconnetworks-com:metadata-1-0/">Majordomo play url</desc>
-                  </item>
-                </DIDL-Lite>';
+        $meta = ';
       
         $args = array('InstanceID'=>0, 'CurrentURI'=>'<![CDATA['.$url.']]>', 'CurrentURIMetaData'=>$meta);  
         $response = $this->sendRequestToDevice('SetAVTransportURI',$args,$this->ctrlurl,$this->service_type);
