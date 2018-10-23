fc2a859f7add5236290b2f434d865bb4a74a6ed6
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-23 09:28:44 +0300

    Update Remote.php

--- modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
+++ modules/ssdp_finder/upnp/vendor/jalder/upnp/src/MediaRenderer/Remote.php
@@ -95,19 +95,8 @@ class Remote {
         }
         $content_type = get_headers($url, 1)["Content-Type"];
         var_dump($content_type);
-    	$MetaData='&lt;?xml version="1.0" encoding="UTF-8"?&gt;
-&lt;DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/" xmlns:sec="http://www.sec.co.kr/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/"&gt;
-&lt;item id="0" parentID="0" restricted="1"&gt;
-&lt;upnp:class&gt;object.item.audioItem.musicTrack&lt;/upnp:class&gt;
-&lt;dc:title&gt;Live Audio&lt;/dc:title&gt;
-&lt;dc:creator&gt;PulseAudio&lt;/dc:creator&gt;
-&lt;upnp:artist&gt;PulseAudio on bozo-laptop&lt;/upnp:artist&gt;
-&lt;upnp:albumArtURI&gt;&lt;/upnp:albumArtURI&gt;
-&lt;upnp:album&gt;Stream&lt;/upnp:album&gt;
-&lt;res protocolInfo="http-get:*:'.$content_type.':DLNA.ORG_OP=00;DLNA.ORG_FLAGS=017000000000000 00000000000000000"&gt;' . $url . '&lt;/res&gt;
-&lt;/item&gt;
-&lt;/DIDL-Lite&gt;';
-        $args = array('InstanceID' => 0, 'CurrentURI' => '<![CDATA[' . $url . ']]>', 'CurrentURIMetaData' => $MetaData);
+    	$MetaData='&lt;?xml version="1.0" encoding="UTF-8"?&gt;&lt;DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/" xmlns:sec="http://www.sec.co.kr/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/"&gt;&lt;item id="0" parentID="0" restricted="1"&gt;&lt;upnp:class&gt;object.item.audioItem.musicTrack&lt;/upnp:class&gt;&lt;dc:title&gt;Live Audio&lt;/dc:title&gt;&lt;dc:creator&gt;PulseAudio&lt;/dc:creator&gt;&lt;upnp:artist&gt;PulseAudio on bozo-laptop&lt;/upnp:artist&gt;&lt;upnp:albumArtURI&gt;&lt;/upnp:albumArtURI&gt;&lt;upnp:album&gt;Stream&lt;/upnp:album&gt;&lt;res protocolInfo="http-get:*:audio/mpeg:DLNA.ORG_OP=00;DLNA.ORG_FLAGS=017000000000000 00000000000000000"&gt;' . $url . '&lt;/res&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;';
+	$args = array('InstanceID' => 0, 'CurrentURI' => '<![CDATA[' . $url . ']]>', 'CurrentURIMetaData' => $MetaData);
         $response = $this->sendRequestToDevice('SetAVTransportURI', $args);
         var_dump($response);
         $args = array( 'InstanceID' => 0, 'Speed' => 1);
