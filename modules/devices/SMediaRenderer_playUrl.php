f719ff9725089aeef48f5bd2a418febc863060a7
tarasfrompir <tarasfrompir@inbox.ru>
2018-10-23 15:52:25 +0300

    Update SMediaRenderer_playUrl.php

--- modules/devices/SMediaRenderer_playUrl.php
+++ modules/devices/SMediaRenderer_playUrl.php
@@ -6,6 +6,5 @@ $renderer = new MediaRenderer();
 $adress = $this->getProperty("CONTROLADDRESS");
 $remote = new MediaRenderer\Remote($adress);
 $playUrl = $this->getProperty("playUrl");
-$result = $remote->stop();
 $result = $remote->play($playUrl);
 DebMes($result);
